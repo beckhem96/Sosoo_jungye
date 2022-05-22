d17f05680816f9de6514f63945b0ca4ad5456163
beckhem96 <beckhem96@naver.com>
2022-05-22 19:03:55 +0900

    관리자 영화 추가 코드 추가

--- front/last-pjt/src/components/DetailView/DetailMovieCommentItem.vue
+++ front/last-pjt/src/components/DetailView/DetailMovieCommentItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>영화가 좀 별로네요....</p>
+    <p>Articles_id와 comment_id 이용 반복문?</p>
   </div>
 </template>
 
---
+++ front/last-pjt/src/views/AdminMovieAdd.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>영화 추가하기</h1>
+    <div>
+      <p>제목</p>
+      <p>내용</p>
+      <p>포스터</p>
+      <p>등등</p>
+      <button>추가하기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminMovieAdd'
+}
+</script>
+
+<style>
+
+</style>
