e624f78ea40f68dc8079eb4899118de697adb8eb
yewon129 <sky25052@gmail.com>
2022-05-26 09:33:35 +0900

    data 및 장고 세팅 변화

--- back/pjt/settings.py
+++ back/pjt/settings.py
@@ -151,10 +151,10 @@ AUTH_USER_MODEL = 'accounts.User'
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
-    ]
 }
